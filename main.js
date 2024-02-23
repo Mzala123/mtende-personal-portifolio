3bf956fea1e2dfd8711c5b00447a9b7cf481cc44
Mzala123 <justicemwanzamj@gmail.com>
2024-02-23 10:35:27 +0200

    responsive header

--- main.js
+++ main.js
@@ -4,12 +4,9 @@ const toggle_open = document.getElementById("open_toggle");
 const navItems = document.querySelector("header div:nth-of-type(2)")
 
 const open_drawer =  () =>{
-
     navItems.classList.contains('menu_open') ? navItems.classList.add('animation') : navItems.classList.remove('animation')
     navItems.classList.toggle('menu_open')
     navItems.classList.toggle('menu_close')
-
-
 }
 
 toggle_open.addEventListener("click", open_drawer)
