c12986f80fe3dc57ae80f2d5c30aebba736e74eb
Mzala123 <justicemwanzamj@gmail.com>
2024-02-29 13:22:13 +0200

    menu dev

--- main.js
+++ main.js
@@ -20,7 +20,25 @@ window.addEventListener("load", ()=>{
             console.log(a.getAttribute('id'))
             const section = document.querySelector(`.${a.getAttribute('id')}`)
             section.scrollIntoView({behavior:"smooth",block:"center"})
+            navItems.classList.toggle('menu_open')
+            navItems.classList.toggle('menu_close')
          })
     })
+
+
+    document.addEventListener('click', function(event) {
+        var isClickInsideMenu = navItems.contains(event.target);
+        var isClickInsideButton = toggle_open.contains(event.target);
+    
+        if (!isClickInsideMenu && !isClickInsideButton) {
+            navItems.classList.toggle('menu_open')
+            navItems.classList.toggle('menu_close')
+        }
+      });
+    
+
+
+
+
    
 })
