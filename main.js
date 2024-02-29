e4bdc93d3deb8bc882956bba613002e304ae3b28
Mzala123 <justicemwanzamj@gmail.com>
2024-02-29 11:23:12 +0200

    scrolling to section

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
-const toggle_close = document.getElementById("close_toggle")
-const toggle_open = document.getElementById("open_toggle");
 
-const navItems = document.querySelector("header div:nth-of-type(2)")
+window.addEventListener("load", ()=>{
 
-const open_drawer =  () =>{
-    navItems.classList.contains('menu_open') ? navItems.classList.add('animation') : navItems.classList.remove('animation')
-    navItems.classList.toggle('menu_open')
-    navItems.classList.toggle('menu_close')
-}
+    const toggle_close = document.getElementById("close_toggle")
+    const toggle_open = document.getElementById("open_toggle");
+    
+    const navItems = document.querySelector("header div:nth-of-type(2)")
+    
+    const open_drawer =  () =>{
+        navItems.classList.contains('menu_open') ? navItems.classList.add('animation') : navItems.classList.remove('animation')
+        navItems.classList.toggle('menu_open')
+        navItems.classList.toggle('menu_close')
+    }
+    
+    toggle_open.addEventListener("click", open_drawer)
 
-toggle_open.addEventListener("click", open_drawer)
-
-
-/* <main>              
-              <div class="intro">
-                  <div>
-                    <h2>Hie <span>I'm Mtende J Mwanza</span></h2>
-                    <h3>Full-Stack Web developer</h3>
-                    <p>Perceptive and result driven Full-Stack Developer, proficient at designing innovative
-                       IT solutions and enhancing existing systems with new features. <br>
-                    </p>
-                    <p>
-                        As a Full-Stack Developer, I am capable of building robust, user friendly and quality 
-                        software within schedule. I have vast experience and knowledge in developing data 
-                        driven web applications and desktop applications.
-                    </p>
-                  </div>
-                  <div>
-                    <picture>
-                        <img src="src/mtende.jpeg" alt="">
-                    </picture>
-                     
-                  </div>
-              </div>
-          </main> */
+    const navigation_links =  document.querySelectorAll("header div:nth-of-type(2) a")
+    navigation_links.forEach((a)=>{
+         a.addEventListener("click", ()=>{
+            console.log(a.getAttribute('id'))
+            const section = document.querySelector(`.${a.getAttribute('id')}`)
+            section.scrollIntoView({behavior:"smooth",block:"center"})
+         })
+    })
+   
+})
